545932c17cc640af58e4b23927e6f6b4c9748571
ZeroDegress <zerodegress@outlook.com>
2023-11-21 14:53:20 +0800

    Update scripts.

--- scripts/Deploy.ps1
+++ scripts/Deploy.ps1
@@ -1,5 +1,5 @@
 $gamePath = Resolve-Path $(Get-Content '.gamepath')
-New-Item -ItemType SymbolicLink -Path $(Join-Path -Path $gamePath -ChildPath "BepInEx\plugins\SFHR_ZModLoader.dll") -Value $(Resolve-Path "bin\Debug\net481\SFHR_ZModLoader.dll") -ErrorAction SilentlyContinue
-New-Item -ItemType SymbolicLink -Path $(Join-Path -Path $gamePath -ChildPath "BepInEx\plugins\Newtonsoft.Json.dll") -Value $(Resolve-Path "bin\Debug\net481\Newtonsoft.Json.dll") -ErrorAction SilentlyContinue
+New-Item -ItemType SymbolicLink -Path $(Join-Path -Path $gamePath -ChildPath "BepInEx\plugins\SFHR_ZModLoader.dll") -Value $(Resolve-Path "bin\Debug\net6.0\SFHR_ZModLoader.dll") -ErrorAction SilentlyContinue
+#New-Item -ItemType SymbolicLink -Path $(Join-Path -Path $gamePath -ChildPath "BepInEx\plugins\Newtonsoft.Json.dll") -Value $(Resolve-Path "bin\Debug\net6.0\Newtonsoft.Json.dll") -ErrorAction SilentlyContinue
 #New-Item -ItemType SymbolicLink -Path $(Join-Path -Path $gamePath -ChildPath "BepInEx\plugins\ClearScript.Core.dll") -Value "bin\Debug\net481\ClearScript.Core.dll"
 #New-Item -ItemType SymbolicLink -Path $(Join-Path -Path $gamePath -ChildPath "BepInEx\plugins\ClearScript.V8.dll") -Value "bin\Debug\net481\ClearScript.V8.dll"
--- scripts/FetchDependencies.ps1
+++ scripts/FetchDependencies.ps1
@@ -3,3 +3,9 @@ New-Item -ItemType Directory -Path 'deps' -ErrorAction SilentlyContinue
 New-Item -ItemType SymbolicLink -Path 'deps/Assembly-CSharp.dll' -Value $(Join-Path -Path $gamePath -ChildPath 'BepInEx/Interop/Assembly-CSharp.dll') -ErrorAction SilentlyContinue
 New-Item -ItemType SymbolicLink -Path 'deps/Assembly-CSharp-firstpass.dll' -Value $(Join-Path -Path $gamePath -ChildPath 'BepInEx/Interop/Assembly-CSharp-firstpass.dll') -ErrorAction SilentlyContinue
 New-Item -ItemType SymbolicLink -Path 'deps/Il2Cppmscorlib.dll' -Value  $(Join-Path -Path $gamePath -ChildPath 'BepInEx/Interop/Il2Cppmscorlib.dll') -ErrorAction SilentlyContinue
+New-Item -ItemType SymbolicLink -Path 'deps/UnityEngine.dll' -Value  $(Join-Path -Path $gamePath -ChildPath 'BepInEx/Interop/UnityEngine.dll') -ErrorAction SilentlyContinue
+New-Item -ItemType SymbolicLink -Path 'deps/UnityEngine.InputModule.dll' -Value  $(Join-Path -Path $gamePath -ChildPath 'BepInEx/Interop/UnityEngine.InputModule.dll') -ErrorAction SilentlyContinue
+New-Item -ItemType SymbolicLink -Path 'deps/UnityEngine.InputLegacyModule.dll' -Value  $(Join-Path -Path $gamePath -ChildPath 'BepInEx/Interop/UnityEngine.InputLegacyModule.dll') -ErrorAction SilentlyContinue
+New-Item -ItemType SymbolicLink -Path 'deps/UnityEngine.CoreModule.dll' -Value  $(Join-Path -Path $gamePath -ChildPath 'BepInEx/Interop/UnityEngine.CoreModule.dll') -ErrorAction SilentlyContinue
+New-Item -ItemType SymbolicLink -Path 'deps/UnityEngine.AudioModule.dll' -Value  $(Join-Path -Path $gamePath -ChildPath 'BepInEx/Interop/UnityEngine.AudioModule.dll') -ErrorAction SilentlyContinue
+New-Item -ItemType SymbolicLink -Path 'deps/UnityEngine.ImageConversionModule.dll' -Value  $(Join-Path -Path $gamePath -ChildPath 'BepInEx/Interop/UnityEngine.ImageConversionModule.dll') -ErrorAction SilentlyContinue
